cb86de574a052ea6dd893e11df20a721a6b85386
Robert <knightrob6819@gmail.com>
2025-11-12 02:40:37 -0500

    Changes to functionality and appearance

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
+const input = document.getElementById("inp");
 
-let string = '';
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) => {
-        if (e.target.innerHTML == '=') {
-            
-            string = eval(string);
-            input.value = string;
-            
+const btns = document.querySelectorAll('button');
+
+let string = ""
+
+btns.forEach(btn => {
+  
+  btn.addEventListener("click", () => {
+    if (btn.innerHTML == "AC") {
+      string = ""
+      input.value = string
+    } else if (btn.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1)
+      input.value = string
+    } else if (btn.innerHTML == "=") { 
+      if (string == "") {
+        return
+      } else {
+        if (string[string.length - 1] == "%") {
+          string = +string.substring(0,string.length - 1) / 100
+          input.value = string
+        } else {
+          string = eval(input.value);
+          input.value = string
         }
-        else if (e.target.innerHTML == 'AC') {
-            string = '';
-            input.value = string;
-        }
-        else if (e.target.innerHTML == 'DEL') {
-            string = string.substring(0, string.length - 1);
-            input.value = string;
-        }
-            
-        else {
-            string += e.target.innerHTML;
-            input.value = string;
-        }
-        
-    })
+      }
+    } else {
+      string = string + btn.innerHTML
+      input.value = string
+    }
+  })
 })
+
